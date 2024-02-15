24cfb2566fd3fe2606702cff072ef757d3398b98
OCangrand <olivier.cangrand@gmail.com>
2024-02-15 17:47:01 +0100

    Avancement sur la partie dashboard

--- Dashboard.py
+++ Dashboard.py
@@ -1,15 +1,15 @@
-import mlflow
 import pandas as pd
 import requests
 import streamlit as st
+import plotly.express as px
+
+st.set_page_config(layout="wide")
 
 st.title("Calcul de probabilité du remboursement d'un prêt")
 
 df = pd.read_csv("dataframe_final_test.csv")
 
 id_user = st.text_input("Entrez l'id du client")
-
-
 
 if id_user:
     try :
@@ -24,13 +24,47 @@ if id_user:
             req = requests.post("http://127.0.0.1:5000/prediction", json=row_json)
             st.write("Passage API OK")
             resultat = req.json()
-            st.write("Probabilité de faillite du client :", resultat["Proba_Faillite"])
-            if resultat["Proba_Faillite"]>0.3:
+            st.write("Probabilité de faillite du client :", round(resultat["Proba_Faillite"], 2),"%")
+            if resultat["Proba_Faillite"]>30:
                 st.write("Prêt refusé...")
             else:
                 st.write("Prêt accepté !")
-            st.write("Shap Values :", resultat["Shap_Values"])
-            st.write("Feature Names :", resultat["Feature_Names"])
-            st.write("Feature Values :", resultat["Feature_Values"])
+
+            dfFeatName = pd.DataFrame(resultat['Feature_Names'])
+            dfFeatValue = pd.DataFrame(resultat['Feature_Values'])
+            dfShapValue = pd.DataFrame(resultat['Shap_Values'])
+            feat_df = pd.concat([dfFeatName, dfFeatValue, dfShapValue], axis=1)
+            feat_df.columns = ['Feature_Names', 'Feature_Values', 'Shap_Values']
+            feat_df['Abs_Shap'] = feat_df['Shap_Values'].abs()
+            feat_df.sort_values(by='Abs_Shap', ascending=False, inplace=True, ignore_index=True)
+            feat_df.drop('Abs_Shap', axis=1, inplace=True)
+
+            st.write(feat_df)
+
+            
+            c1, c2 = st.columns(2)
+
+            c1.title("Analyse univariée")
+            feat_selected_hist = c1.selectbox(label="Selectionner une feature :", options=feat_df['Feature_Names'], index=None)
+            if feat_selected_hist:
+                fig_hist = px.histogram(df[feat_selected_hist], x=feat_selected_hist)
+                feat_selected_hist_serie = feat_df.loc[feat_df['Feature_Names'] == feat_selected_hist, 'Feature_Values']
+                feat_selected_hist = feat_selected_hist_serie.iloc[0]
+
+                fig_hist.add_annotation(x=feat_selected_hist, y=0, text="Client", showarrow=True, arrowhead=1, arrowcolor="Red", bgcolor="Red")
+                c1.plotly_chart(fig_hist)
+
+
+            c2.title("Analyse bivariée")
+            feat_select_scatter_1 = c2.selectbox(label="Selectionner une 1ère feature :", options=feat_df['Feature_Names'], index=None)
+            feat_select_scatter_2 = c2.selectbox(label="Selectionner une 2ème feature :", options=feat_df['Feature_Names'], index=None)
+            if feat_select_scatter_1 and feat_select_scatter_2:
+                fig_scatter = px.scatter(df, x=feat_select_scatter_1, y=feat_select_scatter_2)
+                feat_selected_scatter_serie_1 = feat_df.loc[feat_df['Feature_Names'] == feat_select_scatter_1, 'Feature_Values']
+                feat_selected_scatter_1 = feat_selected_scatter_serie_1.iloc[0]
+                feat_selected_scatter_serie_2 = feat_df.loc[feat_df['Feature_Names'] == feat_select_scatter_2, 'Feature_Values']
+                feat_selected_scatter_2 = feat_selected_scatter_serie_2.iloc[0]
+                fig_scatter.add_annotation(x=feat_selected_scatter_1, y=feat_selected_scatter_2, text="Client", showarrow=True, arrowhead=1, arrowcolor="Red", bgcolor="Red")
+                c2.plotly_chart(fig_scatter)
         else:
-            st.write("Id non trouvé dans la base de donnée. Essayez en un autre (Exemple : 100001, 100028, 456250)")
+            st.write("Id non trouvé dans la base de donnée. Essayez-en un autre (Exemple : 100001, 100005, 100028, 456250).")
